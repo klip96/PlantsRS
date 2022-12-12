887840c761e3dd6d64c4ff3696dbc3c815fb3d4e
klip96 <klip7526146096@gmail.com>
2022-12-12 22:01:00 +0300

    feat: add second part in price section

--- main/script.js
+++ main/script.js
@@ -6,11 +6,27 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
-    }
+      document.querySelectorAll('.panel').forEach((el) => el.classList.remove("active"))
+      panel.style.maxHeight = 108 + "px";
+    } 
   });
 }
+
+// document.querySelectorAll('.accordion').forEach((el) => {
+//   el.addEventListener('click', () => {
+
+//     let content = el.nextElementSibling;
+
+//     if (content.style.maxHeight) {
+//       document.querySelectorAll('.panel').forEach((el) => el.style.maxHeight = null)
+//     } else {
+//       document.querySelectorAll('.panel').forEach((el) => el.style.maxHeight = null)
+//       content.style.maxHeight = content.scrollHeight + "px"
+//     }
+//   })
+// })
+
 
