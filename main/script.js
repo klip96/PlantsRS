971d59c222fecd4e9af9d995d1ab5687a71e9c0b
klip96 <klip7526146096@gmail.com>
2022-12-22 21:57:28 +0300

    feat: add acordion + fix

--- main/script.js
+++ main/script.js
@@ -4,29 +4,24 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      document.querySelectorAll('.panel').forEach((el) => el.classList.remove("active"))
-      panel.style.maxHeight = 108 + "px";
-    } 
+    this.classList.toggle("active").forEach((el) => acc.classList.remove("active"))
+    
+    
   });
 }
 
-// document.querySelectorAll('.accordion').forEach((el) => {
-//   el.addEventListener('click', () => {
+document.querySelectorAll('.accordion').forEach((el) => {
+  el.addEventListener('click', () => {
+    let content = el.nextElementSibling;
 
-//     let content = el.nextElementSibling;
-
-//     if (content.style.maxHeight) {
-//       document.querySelectorAll('.panel').forEach((el) => el.style.maxHeight = null)
-//     } else {
-//       document.querySelectorAll('.panel').forEach((el) => el.style.maxHeight = null)
-//       content.style.maxHeight = content.scrollHeight + "px"
-//     }
-//   })
-// })
+    if (content.style.maxHeight) {
+      
+      document.querySelectorAll('.panel').forEach((el) => el.style.maxHeight = null)
+    } else {
+      document.querySelectorAll('.panel').forEach((el) => el.style.maxHeight = null)
+      content.style.maxHeight = 108 + "px";
+    }
+  })
+})
 
 
