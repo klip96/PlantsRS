b7f389c81bd125684454ea63a51d8fd792e63924
klip96 <klip96@list.ru>
2023-02-04 21:39:53 +0300

    feat: brur ef

--- main/script.js
+++ main/script.js
@@ -20,6 +20,54 @@ for (let i = 0; i < acc.length; i++) {
 }
 
 
+
+
+
+const gardenButt = document.getElementById('garden_but');
+const gardenButt2 = document.getElementById('lawn_but');
+const gardenButt3 = document.getElementById('planting_but');
+const garden1 = document.getElementById('service_1');
+const garden2 = document.getElementById('service_2');
+const garden3 = document.getElementById('service_3');
+const garden4 = document.getElementById('service_4');
+const garden5 = document.getElementById('service_5');
+const garden6 = document.getElementById('service_6');
+
+gardenButt.addEventListener('click', function(){
+	gardenButt.classList.toggle('garden_active')
+  gardenButt3.classList.remove('garden_active')
+  gardenButt2.classList.remove('garden_active')
+  garden2.style.filter = "blur(3px)";
+  garden3.style.filter = "blur(3px)";
+  garden4.style.filter = "blur(3px)";
+  garden6.style.filter = "blur(3px)";
+  garden1.style.filter = "";
+  garden5.style.filter = "";
+
+});
+gardenButt2.addEventListener('click', function(){
+	gardenButt2.classList.toggle('garden_active')
+  gardenButt.classList.remove('garden_active')
+  gardenButt3.classList.remove('garden_active')
+  garden1.style.filter = "blur(3px)";
+  garden2.style.filter = "blur(3px)";
+  garden3.style.filter = "";
+  garden4.style.filter = "blur(3px)";
+  garden5.style.filter = "blur(3px)";
+  garden6.style.filter = "blur(3px)";
+});
+gardenButt3.addEventListener('click', function(){
+	gardenButt3.classList.toggle('garden_active')
+  gardenButt2.classList.remove('garden_active')
+  gardenButt.classList.remove('garden_active')
+  garden1.style.filter = "blur(3px)";
+  garden2.style.filter = "";
+  garden3.style.filter = "blur(3px)";
+  garden4.style.filter = "";
+  garden5.style.filter = "blur(3px)";
+  garden6.style.filter = "";  
+});
+
 const burgerButt = document.querySelector('.menu_burger_header');
 const burger = document.querySelector('.pop_burger');
 const body = document.body;
